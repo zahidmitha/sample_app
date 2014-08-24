03c98539032f3778fc45dd42fcf4d365ffc26ed6
zahidmitha <zahidazimmitha@gmail.com>
2014-08-24 23:49:05 +0200

    ch7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,15 @@ describe "User pages" do
       let(:submit) { "Create my account" }
 
       describe "with invalid information" do
+
+        describe "after submission" do
+                before { click_button submit }
+
+                it { should have_title('Sign up') }
+                it { should have_content('error') }
+        end
+
+
         it "should not create a user" do
           expect { click_button submit }.not_to change(User, :count)
         end
@@ -38,6 +47,14 @@ describe "User pages" do
           fill_in "Email",        with: "user@example.com"
           fill_in "Password",     with: "foobar"
           fill_in "Password confirmation", with: "foobar"
+        end
+
+        describe "after saving the user" do
+                before { click_button submit }
+                let(:user) { User.find_by(email: 'user@example.com') }
+
+                it { should have_title(user.name) }
+                it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         end
 
         it "should create a user" do
