f35809acc19792b1b45397e7262f308d074ddc37
zahidmitha <zahidazimmitha@gmail.com>
2014-09-04 10:47:24 +0200

    added security for signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,21 @@
       else
         render 'new'
       end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
    private
 
