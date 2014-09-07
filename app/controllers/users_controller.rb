019d03182e81179423b05e579ec8d5899bff9ba9
zahidmitha <zahidazimmitha@gmail.com>
2014-09-07 22:15:02 +0200

    Added correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
   	@user = User.new
@@ -22,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -35,13 +34,24 @@ class UsersController < ApplicationController
     end
   end
 
-   private
+  private
 
    	def user_params
    		params.require(:user).permit(:name, :email, :password, :password_confirmation)
    	end
 
+    # Before filters
+
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+          store_location
+          redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
     end
+
+    def correct_user
+          @user = User.find(params[:id])
+          redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
