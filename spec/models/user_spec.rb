881bb7ed24a7c073ec6a8221781b07be66713dee
zahidmitha <zahidazimmitha@gmail.com>
2015-03-04 11:59:14 +0300

    Added tests for micropost associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -109,6 +110,21 @@ describe User do
     describe "remember token" do
       before { @user.save }
       its(:remember_token) { should_not be_blank }
+    end
+
+    describe "micropost associations" do
+
+      before { @user.save }
+      let!(:older_micropost) do
+        FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+      end
+      let!(:newer_micropost) do
+        FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+      end
+
+      it "should have the right microposts in the right order" do
+        expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+      end
     end
 
 end
